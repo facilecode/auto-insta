cd4fb566664e5085131813f893eae3f05e53d900
EmmZak <emmanuelzakaryan@gmail.com>
2021-03-02 14:57:34 +0100

    refactoring

--- main.js
+++ main.js
@@ -4,14 +4,22 @@ pup.launch({
     headless: false
 })
 .then(async browser => {
+
     let url = 'https://www.instagram.com'
 
+    // opening page
     let page = await browser.newPage()
     await page.goto(url, {waitUntil: 'networkidle2' })
 
+    // accepting cookies
     const [button] = await page.$x("//button[text()='Accept']")
     console.log('button -> ', button)
     await button.click()
+
+    // Log In
+
+    // Go to hashtag page from file.txt
+    
 
 })
 .catch(err => console.log('err - ', err))
