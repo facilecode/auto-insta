6bf21bc7fadd53b0ad98cfac877d4f0730e7e086
Delphin <delphinrukundo10@gmail.com>
2021-03-08 22:20:34 +0100

    like single image

--- insta.js
+++ insta.js
@@ -64,17 +64,47 @@ const insta = {
     
     //const links = await insta.page.evaluate(() => Array.from(document.querySelectorAll("article > div > div > div > div > a > div > div > img"), e => e));
     // const links = await insta.page.$("article > div > div > div > div > a > div > div > img");
-    const links = await insta.page.$$('img[style="object-fit: cover;"]');
     //const n_links = links.slice(0, n);
-    await insta.page.waitForTimeout(2000);
-
+    await insta.page.waitForTimeout(1000);
+    
     // scroll down
     await insta.page.mouse.wheel({ deltaY: 1000 });
+    
+    await insta.page.waitForTimeout(2000);
+
+    const links = await insta.page.$$('img[style="object-fit: cover;"]');
+
+    // make sure that n is not greater than links.length
+    // get n random images from the links images
+    const randomImgIndeces = Array.from({length: n}, () => Math.floor(Math.random() * n));
+
+    // get only unique images
+    const uniqueImg = [...new Set(randomImgIndeces)];
+
+    
+    uniqueImg.forEach((img) => {
+      // like the image
+      insta.likeSingleImage(links[img]);
+
+      // wait for 2 seconds
+      insta.page.waitForTimeout(2000);
+
+      // close the image preview
+      // insta.closeImagePreview();
+    });
+
+
+    for (let i = 0; i < n; i++) {
+      let randImg = Math.floor(Math.random() * links.length);
+
+      links[randImg].click();
+    }
+    
     debugger;
     // wait for images to load
 
     // All images
-    //document.querySelectorAll('img[style="object-fit: cover;"]')
+    // document.querySelectorAll('img[style="object-fit: cover;"]')
 
     
     // await insta.page.waitForTimeout(2000);
@@ -97,6 +127,27 @@ const insta = {
 
     
   },
+
+  likeSingleImage: async (img) => {
+
+    // open image preview
+    img.click();
+
+    // wait for 2 seconds
+    await insta.page.waitForTimeout(2000);
+
+     /**
+      * by selecting a publication via 'Like' label, 
+      * we ensure that a liked image won't be unliked 
+      * when image is already liked aria-label = Unlike
+      */
+    await insta.page.waitForSelector("svg[aria-label='Like']");
+    const likeSvg = await insta.page.$("svg[aria-label='Like']");
+    const likeButton = await likeSvg.getProperty('parentNode');
+
+    // like the image
+    likeButton.click();
+  }
 
 }
 
