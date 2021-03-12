5ff26c656e790017daadcca57d205d0e4aafe2dd
EmmZak <emmanuelzakaryan@gmail.com>
2021-03-12 22:10:59 +0100

    adds reading hashtags  from file and liking n images

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 require('dotenv').config()
 
+const fs = require('fs')
 const insta = require('./insta');
 const username = process.env.INSTA_USERNAME;
 const password = process.env.INSTA_PASSWORD;
 
 (async () => {
-
+    
     // initialize the page
     await insta.initialize();
 
@@ -19,9 +20,15 @@ const password = process.env.INSTA_PASSWORD;
     await insta.page.waitForTimeout(4000);
 
     // refuse save account
+    // TO-DO
 
     // refuse notifications
-    const tags = ['gyumritheatre', 'javascript', 'python'];
+    // TO-DO
+
+    // read hashtags from file
+    let tags = fs.readFileSync('hashtags.txt').toString().split('\n');
+    tags = tags.map(tag => tag.replace(/(\r\n|\n|\r)/gm, ""));
+    console.log('hashtags ', tags);
     
     //await insta.goToHashtagPage(tags[0]);
     // like images
